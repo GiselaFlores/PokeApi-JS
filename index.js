cdb60556a8df69733e98152cebf4a92af62e491c
GiselaFlores <gi.flores@bue.edu.ar>
2024-11-14 20:10:15 -0300

    codigo div

---
+++ index.js
@@ -0,0 +1,56 @@
+// nodos del html, contenedor y los botones
+const listaPokemon = document.querySelector("#listaPokemon");
+const botonesHeader = document.querySelectorAll(".btn-header");
+
+let URL = "https://pokeapi.co/api/v2/pokemon/";
+
+// comunicarnos con la API
+// Generar los numeros de poke de la API y derivarla a la función creadora de la card
+
+// ejemplo: "https://pokeapi.co/api/v2/pokemon/151"
+
+for(let i; i <=151; i++){
+    fetch(URL + i)
+        .then((response)=> response.json())
+        .then(data => mostrarPokemon(data))
+}
+
+//funcion que genera la card
+
+function mostrarPokemon(poke){
+
+    const div = document.createElement("div");
+    div.classList.add("pokemon");
+
+    let tipos = poke.types.map((type)=> `<p class="${type.type.name} tipo" >${type.type.name} </p>`);
+    tipos = tipos.join("");
+
+    let pokeId = poke.id.toString();
+    if(pokeId.length === 1){
+        pokeId = "00" + pokeId;
+    } else if(pokeId.length === 2){
+        pokeId = "0" + pokeId;
+    }
+
+    div.innerHTML = `
+        <p class="pokemon-id-back">#${pokeId}</p>
+        <div class="pokemon-imagen">
+            <img src="${poke.sprites.other["official-artwork"].front_default}" alt="${poke.name}">
+        </div>
+        <div class="pokemon-info">
+            <div class="nombre-contenedor">
+                <p class="pokemon-id">#${pokeId}</p>
+                <h2 class="pokemon-nombre">${poke.name}</h2>
+            </div>
+            <div class="pokemon-tipos">
+                ${tipos}
+            </div>
+            <div class="pokemon-stats">
+                <p class="stat">${poke.height}m</p>
+                <p class="stat">${poke.weight}kg</p>
+            </div>
+        </div>
+    `;
+
+    listaPokemon.append(div);
+}
