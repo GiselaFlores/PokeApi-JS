0018630ef35269d24f7027798176b48ac2262fcc
GiselaFlores <gi.flores@bue.edu.ar>
2024-11-14 21:58:25 -0300

    final

--- index.js
+++ index.js
@@ -50,10 +50,31 @@ function mostrarPokemon(poke) {
                 <p class="stat">${poke.height}m</p>
                 <p class="stat">${poke.weight}kg</p>
             </div>
+            
         </div>
     `;
 
     listaPokemon.append(div);
 }
 
+botonesHeader.forEach(boton => boton.addEventListener("click", (e)=>{
+    const botonId = e.currentTarget.id;
 
+    listaPokemon.innerHTML = "";
+
+    for (let i = 1; i <= 200; i++) {
+        fetch(URL + i)
+            .then((response) => response.json())
+            .then(data => {
+                if(botonId === "ver-todos"){
+                    mostrarPokemon(data);
+                } else {
+                    const tipos = data.types.map( type => type.type.name);
+                    if(tipos.some(tipo => tipo.includes(botonId))){
+                        mostrarPokemon(data);
+                    }
+                }
+            })
+    }
+
+}))
